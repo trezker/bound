138e0300420e0e3f2765fb14b5e0aa23b74e5250
Anders Andersson <trezker@gmail.com>
2018-08-14 19:48:31 +0200

    Using testsuite

--- source/Test.d
+++ source/Test.d
@@ -3,6 +3,30 @@ module test;
 import std.string;
 import core.exception;
 
+alias Test = void delegate();
+class TestSuite {
+	private Test[] tests;
+
+	void Setup() {}
+	void Teardown() {}
+
+	void Run() {
+		foreach (test; tests) {
+			try {
+				Setup();
+				test();
+			}
+			finally {
+				Teardown();
+			}
+		}
+	}
+
+	void AddTest(Test test) {
+		tests ~= test;
+	}
+}
+
 template assertOp(string op) {
 	void assertOp(T1, T2)(
 		T1 lhs, 
--- source/UserCreator.d
+++ source/UserCreator.d
@@ -24,25 +24,42 @@ class UserCreator {
 	}
 }
 
-unittest {
-	NewUser newUser = {
-		name: "Test"
-	};
-	auto userCreator = new UserCreator;
-	userCreator.Add(newUser);
+class Test: TestSuite {
+	this() {
+		AddTest(&AddUser);
+	}
+
+	override void Setup() {
+	}
+
+	override void Teardown() {
+	}
+
+	void AddUser() {
+		NewUser newUser = {
+			name: "Test"
+		};
+		auto userCreator = new UserCreator;
+		userCreator.Add(newUser);
+	}
+	
+	void UserEntity() {
+		User user = {
+			name: "Test"
+		};
+	}
+
+	void AddUserStore() {
+		auto userStore = new UserStore;
+		NewUser newUser = {
+			name: "Test"
+		};
+		userStore.Add(newUser);
+		assertEqual("Test", userStore.FindByName("Test").name);
+	}
 }
 
 unittest {
-	User user = {
-		name: "Test"
-	};
-}
-
-unittest {
-	auto userStore = new UserStore;
-	NewUser newUser = {
-		name: "Test"
-	};
-	userStore.Add(newUser);
-	assertEqual("Test", userStore.FindByName("Test").name);
+	auto test = new Test;
+	test.Run();
 }
