8f4866b06fc515ae9ef58b48e6aa5d0ec0b5c71b
Anders Andersson <trezker@gmail.com>
2018-09-23 21:47:13 +0200

    CreateSession

---
+++ source/Interactors/CreateSession.d
@@ -0,0 +1,45 @@
+module interactors.CreateSession;
+
+import test;
+import std.uuid;
+import entities.Session;
+
+class CreateSession {
+	SessionStore sessionStore;
+
+	UUID opCall() {
+		auto sessionCreated = SessionCreated(randomUUID, UUID.init);
+		sessionStore.Created(sessionCreated);
+		return sessionCreated.uuid;
+	}
+}
+
+class Test: TestSuite {
+	CreateSession createSession;
+	SessionStore sessionStore;
+
+	this() {
+		AddTest(&CreateSession_creates_session_and_returns_uuid);
+	}
+
+	override void Setup() {
+		sessionStore = new SessionStore;
+
+		createSession = new CreateSession;
+		createSession.sessionStore = sessionStore;
+	}
+
+	void CreateSession_creates_session_and_returns_uuid() {
+		UUID sessionUUID = createSession();
+
+		assertNotEqual(sessionUUID, UUID.init);
+
+		auto currentUser = sessionStore.FindByUUID(sessionUUID);
+		assertEqual(1, currentUser.length);
+	}
+}
+
+unittest {
+	auto test = new Test;
+	test.Run();
+}
