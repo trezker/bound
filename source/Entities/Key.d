aa58102faecf60e837768080144cd5e06d219900
Anders Andersson <trezker@gmail.com>
2018-09-24 11:04:12 +0200

    Created key

--- source/Entities/Key.d
+++ source/Entities/Key.d
@@ -2,9 +2,16 @@ module entities.Key;
 
 import std.uuid;
 import std.algorithm;
+import test;
 
 struct NewKey {
 	string lockUUID;
+	string value;
+}
+
+struct KeyCreated {
+	string uuid;
+	string lock;
 	string value;
 }
 
@@ -25,7 +32,46 @@ class KeyStore {
 		);
 	}
 
+	void Created(KeyCreated keyCreated) {
+		keys ~= Key(
+			keyCreated.uuid,
+			keyCreated.lock,
+			keyCreated.value
+		);
+	}
+
 	Key[] FindByLockUUID(string lockUUID) {
 		return keys.find!((a, b) => a.lockUUID == b)(lockUUID);
 	}
+}
+
+
+class Test: TestSuite {
+	this() {
+		AddTest(&Created_stores_a_new_key);
+	}
+
+	override void Setup() {
+	}
+
+	override void Teardown() {
+	}
+
+	void Created_stores_a_new_key() {
+		auto keyStore = new KeyStore;
+		KeyCreated keyCreated = {
+			uuid: randomUUID.toString,
+			lock: randomUUID.toString,
+			value: "value"
+		};
+		keyStore.Created(keyCreated);
+
+		auto keys = keyStore.FindByLockUUID(keyCreated.lock);
+		assertEqual(1, keys.length);
+	}
+}
+
+unittest {
+	auto test = new Test;
+	test.Run();
 }
