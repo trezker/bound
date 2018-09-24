378a9805f948e80bdf4a99508c5a938e3567780d
Anders Andersson <trezker@gmail.com>
2018-09-24 02:23:13 +0200

    Tests for session store

--- source/Entities/Session.d
+++ source/Entities/Session.d
@@ -3,6 +3,7 @@ module entities.Session;
 import std.uuid;
 import std.algorithm;
 import std.array;
+import test;
 
 struct SessionCreated {
 	UUID uuid;
@@ -31,4 +32,48 @@ class SessionStore {
 	Session[] FindByUUID(UUID uuid) {
 		return sessions.find!((a) => a.uuid == uuid)();
 	}	
+}
+
+class Test: TestSuite {
+	this() {
+		AddTest(&Created_stores_a_new_session);
+		AddTest(&Deleted_removes_session);
+	}
+
+	override void Setup() {
+	}
+
+	override void Teardown() {
+	}
+
+	void Created_stores_a_new_session() {
+		auto sessionStore = new SessionStore;
+		SessionCreated newSession = {
+			uuid: randomUUID,
+			useruuid: randomUUID,
+		};
+		sessionStore.Created(newSession);
+
+		auto sessions = sessionStore.FindByUUID(newSession.uuid);
+		assertEqual(1, sessions.length);
+	}
+
+	void Deleted_removes_session() {
+		auto sessionStore = new SessionStore;
+		SessionCreated newSession = {
+			uuid: randomUUID,
+			useruuid: randomUUID,
+		};
+		sessionStore.Created(newSession);
+
+		sessionStore.Deleted(newSession.uuid);
+
+		auto sessions = sessionStore.FindByUUID(newSession.uuid);
+		assertEqual(0, sessions.length);
+	}
+}
+
+unittest {
+	auto test = new Test;
+	test.Run();
 }
